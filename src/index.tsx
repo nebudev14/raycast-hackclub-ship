110e235c5417355195e6ded687f68a350cc339b4
warren yun <wyun2006@gmail.com>
2022-08-19 04:04:36 -0400

    grab any file

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
-import { Form, ActionPanel, Action, showToast, getPreferenceValues } from "@raycast/api";
+import { Form, ActionPanel, Action, showToast, showInFinder, Clipboard, getPreferenceValues, getSelectedFinderItems } from "@raycast/api";
 import { WebClient } from "@slack/web-api";
+import { homedir } from "os"; 
+import { join } from "path";
+import { useRef } from "react";
 
 type Values = {
-  // textfield: string;
+  textfield?: string;
   textarea: string;
   // datepicker: Date;
   // checkbox: boolean;
@@ -13,14 +16,26 @@ type Values = {
 export default function Command() {
   const { token } = getPreferenceValues();
   const web = new WebClient(token);
+  const infile = useRef(null);
 
   async function handleSubmit(values: Values) {
-    const channelId = 'C029NP7J5C7';
-    const result = await web.chat.postMessage({
-      text: values.textarea,
-      channel: channelId
-    })
-    console.log(`sent ${result.ts}`)
+    const channelId = "C029NP7J5C7";
+
+    try {
+      const images = await getSelectedFinderItems();
+    } catch (e) {
+      console.log(e)
+    }
+
+
+    // const result = await web.chat.postMessage({
+    //   text: values.textarea,
+    //   channel: channelId,
+      
+    // })
+    // console.log(`sent ${result.ts}`)
+    // console.log(values.textfield);
+    // console.log(item)
     // showToast({ title: "Submitted form", message: "See logs for submitted values" });
   }
 
@@ -32,18 +47,20 @@ export default function Command() {
         </ActionPanel>
       }
     >
-      <Form.Description text="This form showcases all available form elements." />
-      {/* <Form.TextField id="textfield" title="Text field" placeholder="Enter text" defaultValue="Raycast" /> */}
-      <Form.TextArea id="textarea" title="Text area" placeholder="Enter multi-line text" />
-      {/* <Form.Separator />
-      <Form.DatePicker id="datepicker" title="Date picker" />
-      <Form.Checkbox id="checkbox" title="Checkbox" label="Checkbox Label" storeValue />
-      <Form.Dropdown id="dropdown" title="Dropdown">
-        <Form.Dropdown.Item value="dropdown-item" title="Dropdown Item" />
-      </Form.Dropdown>
-      <Form.TagPicker id="tokeneditor" title="Tag picker">
-        <Form.TagPicker.Item value="tagpicker-item" title="Tag Picker Item" />
-      </Form.TagPicker> */}
+      <Form.Description text="Ship a project to the Hack Club Slack!" />
+      <Form.TextField id="textfield" title="Title (optional)" placeholder="Title" />
+      <Form.TextArea
+        id="textarea"
+        title="Ship message"
+        placeholder="Detail your ship! Feel free to add demo links too!"
+      />
+      <Form.Separator />
+      {/* <Form.Dropdown id="dropdown" title="Channel">
+        <Form.Dropdown.Item value="C01504DCLVD" title="Scrapbook" />
+        <Form.Dropdown.Item value="C01504DCLVD" title="Scrapbook" />
+        <Form.Dropdown.Item value="C029NP7J5C7" title="warren's channel rofl" />
+      </Form.Dropdown> */}
+
     </Form>
   );
 }
