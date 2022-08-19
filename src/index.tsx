547602824dcfd44e17cc5dd41c5737ac0de11bbc
warren yun <wyun2006@gmail.com>
2022-08-19 01:54:26 -0400

    basic message sending W

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,27 @@
-import { Form, ActionPanel, Action, showToast } from "@raycast/api";
+import { Form, ActionPanel, Action, showToast, getPreferenceValues } from "@raycast/api";
+import { WebClient } from "@slack/web-api";
 
 type Values = {
-  textfield: string;
+  // textfield: string;
   textarea: string;
-  datepicker: Date;
-  checkbox: boolean;
-  dropdown: string;
-  tokeneditor: string[];
+  // datepicker: Date;
+  // checkbox: boolean;
+  // dropdown: string;
+  // tokeneditor: string[];
 };
 
 export default function Command() {
-  function handleSubmit(values: Values) {
-    console.log(values)
-    showToast({ title: "Submitted form", message: "See logs for submitted values" });
+  const { token } = getPreferenceValues();
+  const web = new WebClient(token);
+
+  async function handleSubmit(values: Values) {
+    const channelId = 'C029NP7J5C7';
+    const result = await web.chat.postMessage({
+      text: values.textarea,
+      channel: channelId
+    })
+    console.log(`sent ${result.ts}`)
+    // showToast({ title: "Submitted form", message: "See logs for submitted values" });
   }
 
   return (
@@ -24,9 +33,9 @@ export default function Command() {
       }
     >
       <Form.Description text="This form showcases all available form elements." />
-      <Form.TextField id="textfield" title="Text field" placeholder="Enter text" defaultValue="Raycast" />
+      {/* <Form.TextField id="textfield" title="Text field" placeholder="Enter text" defaultValue="Raycast" /> */}
       <Form.TextArea id="textarea" title="Text area" placeholder="Enter multi-line text" />
-      <Form.Separator />
+      {/* <Form.Separator />
       <Form.DatePicker id="datepicker" title="Date picker" />
       <Form.Checkbox id="checkbox" title="Checkbox" label="Checkbox Label" storeValue />
       <Form.Dropdown id="dropdown" title="Dropdown">
@@ -34,7 +43,7 @@ export default function Command() {
       </Form.Dropdown>
       <Form.TagPicker id="tokeneditor" title="Tag picker">
         <Form.TagPicker.Item value="tagpicker-item" title="Tag Picker Item" />
-      </Form.TagPicker>
+      </Form.TagPicker> */}
     </Form>
   );
 }
